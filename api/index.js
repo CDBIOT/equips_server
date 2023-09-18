3e2d01b4400d275d8054b0794da5e08bc786829f
Cristiano Bento <crisdb2008@gmail.com>
2023-09-18 12:03:53 -0300

    try

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-<<<<<<< HEAD
 const route = express.Router("./rotas_equips, ./rotas_user ");
 const Equips = require("../db_equips")
 const Person = require('../db_user')
@@ -19,36 +18,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) =>{
-=======
-const mongoose = require('mongoose')
-const route = express.Router('../rotas_user');
-const Equips = require('../db_equips')
-const Person = require('../db_user')
-const db = require('../mongoConect')
-
-require('dotenv').config()
-
-const cors = require('cors')
-  
-
-route.use(cors());
-
-route.use((req, res, next) => {
-    console.log("Cors habilitado");
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Header",'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods','GET,PUT, POST, PATCH, DELETE');
-        res.status(200).send({})
-    }
-
-    next()
-    })
-    
-//Read
-route.get('/', (req, res) =>{
     try{
->>>>>>> a79cb75b69e74732d575b7b44292ce8545796699
         res.json({
             sucess: true,
             message: "Backend Equips_server ok!"
@@ -56,7 +26,8 @@ route.get('/', (req, res) =>{
         })}catch(error){
         res.status(500).json({error: error})
     }
-})
+}
+)
 
 //Read
 app.get('/equips', getEquips)
@@ -90,27 +61,8 @@ app.get('/user', async (req, res) =>{
 })
 
     
-<<<<<<< HEAD
 
 app.use('/', express.static(__dirname + '/'))
-=======
- //Create temps
- route.post('/equips', async (req, res) =>{
-    const {patrimonio, equipamento, marca, modelo, serial } = req.body
-       // const temps = req.params
-    const equips = {patrimonio, equipamento, marca, modelo, serial }
-    const create_equip = new Equips(req.body);
-    //temps.save()
-        try{
-            await Equips.create(equips)
-            //temps.save()
-            console.log(equips)
-            res.status(201).json({message: "Equipamento cadastrado com sucesso"})
-            }catch(error){
-            res.status(500).json({error: error})
-        }  
-    })
->>>>>>> a79cb75b69e74732d575b7b44292ce8545796699
     
     
 
